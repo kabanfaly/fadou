73651884539f35ee33333b7e3f9e870262ad6140
KABA N'Faly <nfalykaba@gmail.com>
2016-10-23 23:06:32 -0400

    Lombok integration

---
+++ fadou-jpa/src/main/java/com/fadou/entity/Student.java
@@ -0,0 +1,29 @@
+package com.fadou.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author kaba
+ */
+@Entity
+@Table(name = "student")
+@Data
+public class Student implements Serializable{
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
